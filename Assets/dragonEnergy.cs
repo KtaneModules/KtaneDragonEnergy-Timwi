8a3916d1227aa145ea8aca951de3b122e51fbd75
AAces <djp0908@yahoo.com>
2018-07-12 16:47:25 -0600

    Some code

--- Assets/dragonEnergy.cs
+++ Assets/dragonEnergy.cs
@@ -1,16 +1,146 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+using Newtonsoft.Json;
+using KMHelper;
+using System;
+using Random = UnityEngine.Random;
 using UnityEngine;
 
 public class dragonEnergy : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private static int _moduleIdCounter = 1;
+    public KMAudio newAudio;
+    public KMBombModule module;
+    public KMBombInfo info;
+    private int _moduleId = 0;
+    private bool _isSolved = false, _lightsOn = false;
+
+    public KMSelectable submit, left, right;
+
+    void Start()
+    {
+        _moduleId = _moduleIdCounter++;
+        module.OnActivate += Activate;
+    }
+
+    void Activate()
+    {
+        Init();
+        _lightsOn = true;
+    }
+
+    private void Awake()
+    {
+        submit.OnInteract += delegate
+        {
+            handleSubmit();
+            return false;
+        };
+        left.OnInteract += delegate
+        {
+            handleLeft(); 
+            return false;
+        };
+        right.OnInteract += delegate
+        {
+            handleRight();
+            return false;
+        };
+    }
+
+    void Init()
+    {
+
+    }
+
+    void handleSubmit()
+    {
+
+    }
+    
+    void handleLeft()
+    {
+
+    }
+
+    void handleRight()
+    {
+
+    }
+}
+
+public class Word
+{
+    private GameObject sprite;
+    private Position position;
+    private String name;
+
+    public Word(GameObject sprite, Position pos, String name)
+    {
+        this.sprite = sprite;
+        this.position = pos;
+        this.name = name;
+    }
+
+    static void Swap(Word first, Word second)
+    {
+        Position temp = first.getPosition();
+        first.setPosition(second.getPosition());
+        second.setPosition(temp);
+    }
+
+    void display(int location)
+    {
+
+    }
+
+    Position getPosition()
+    {
+        return this.position;
+    }
+
+    GameObject getSprite()
+    {
+        return this.sprite;
+    }
+
+    String getWord()
+    {
+        return this.name;
+    }
+
+    void setPosition(Position pos)
+    {
+        this.position = pos;
+    }
+}
+
+public class Position
+{
+    Level level;
+    Circle circle;
+
+    public Position(Level level, Circle circle)
+    {
+        this.level = level;
+        this.circle = circle;
+    }
+}
+
+public enum Level
+{
+    EXCLUDED,
+    SECONDARY,
+    TERTIARY,
+    QUARTERNARY
+}
+
+public enum Circle
+{
+    GREEN,
+    PURPLE,
+    RED,
+    BLUE
 }
